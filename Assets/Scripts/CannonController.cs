e4dd025def8a331644ee201615c85217cf0165ec
Artem <armastra@gmail.com>
2022-10-30 18:35:33 +0700

    Сделал прототип системы расчета траектории

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -9,7 +9,10 @@ using UnityEngine.UI;
 public class CannonController : MonoBehaviour
 {
     private const string MOUSEAXIS = "Fire1";
+    private const string BOUNCYWALLLAYER = "BouncyWall";
+    private const string STICKYWALLLAYER = "StickyWall";
     private const float DEG90 = 90f;
+    private const int MAXRECURSIONSTEP = 4;
 
     [SerializeField] private Camera mainCam;
     [SerializeField] private float degreeBorder = 60f;
@@ -17,6 +20,7 @@ public class CannonController : MonoBehaviour
     [SerializeField] private Transform bubbleSpawnPoint;
     [SerializeField] private Image currentBall;
     [SerializeField] private Image nextBall;
+    [SerializeField] private LayerMask ignoreLayers;
 
     private bool cannonIsMoving = false;
     private LineRenderer lineRenderer;
@@ -43,7 +47,7 @@ public class CannonController : MonoBehaviour
             Touch touch = Input.GetTouch(0);
             Debug.Log("Touch Position : " + touch.position);
             cannonIsMoving = true;
-            LineRendererUpdate();
+            LineRendererUpdate(false);
             return;
         }
         // Для ПК
@@ -55,15 +59,19 @@ public class CannonController : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, 0f,
                 Mathf.Clamp(Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg - DEG90, -degreeBorder, degreeBorder));
             cannonIsMoving = true;
-            LineRendererUpdate();
+            LineRendererUpdate(false);
             return;
         }
+        // Отпущена кнопка/палец
         if (cannonIsMoving)
         {
             cannonIsMoving = false;
             Shoot();
             transform.rotation = Quaternion.identity;
+            currentColor = nextColor;
+            nextColor = GenerateColor();
             LineRendererUpdate();
+            UpdateUI();
         }
     }
 
@@ -84,11 +92,11 @@ public class CannonController : MonoBehaviour
         Bubble bubble = Instantiate(BubbleGraph.instance.BubblePrefab, bubbleSpawnPoint.position, Quaternion.identity, bubbleSpawnRoot).
             GetComponent<Bubble>();
         bubble.Initialize(true, transform.rotation * Vector2.up, currentColor);
-        currentColor = nextColor;
-        nextColor = GenerateColor();
-        UpdateUI();
     }
 
+    /// <summary>
+    /// Обновление UI
+    /// </summary>
     private void UpdateUI()
     {
         currentBall.color = BubbleGraph.instance.GetColorByEnum(currentColor);
@@ -98,11 +106,55 @@ public class CannonController : MonoBehaviour
     /// <summary>
     /// Обновление визуального положения линии-траектории
     /// </summary>
-    private void LineRendererUpdate()
+    private void LineRendererUpdate(bool setDefaults = true)
     {
-        lineRenderer.SetPositions(new Vector3[] { 
+        Vector3[] newValues = new Vector3[] {
             lineRenderer.GetPosition(0),
+            bubbleSpawnPoint.position,
+            bubbleSpawnPoint.position,
+            bubbleSpawnPoint.position,
             bubbleSpawnPoint.position
-        });
+        };
+
+        if (setDefaults)
+        {
+            lineRenderer.SetPositions(newValues);
+            return;
+        }
+        RaycastHit2D hit = Physics2D.Raycast(bubbleSpawnPoint.position, bubbleSpawnPoint.up, 
+            float.MaxValue);
+        newValues = ReflectRay(hit, bubbleSpawnPoint.up, newValues);
+        lineRenderer.SetPositions(newValues);
+    }
+
+    /// <summary>
+    /// Подсчет траектории выстрела после n-ного рикошета
+    /// </summary>
+    /// <param name="hit">Объект, которого пузырь коснется последним</param>
+    /// <param name="direction">Направление полёта пузыря</param>
+    /// <param name="poins">Точки соприкосновения</param>
+    /// <param name="recursionStep">Текущий шаг рекурсии</param>
+    /// <returns>true, если пуля попадёт по игроку (иначе false)</returns>
+    private Vector3[] ReflectRay(RaycastHit2D hit, Vector2 direction, Vector3[] points, int recursionStep = 1)
+    {
+        if (hit.collider == null || recursionStep > MAXRECURSIONSTEP)
+        {
+            return points;
+        }
+        // Выстрел по препятствию - отражение луча
+        if (hit.collider.gameObject.layer == LayerMask.NameToLayer(STICKYWALLLAYER) ||
+            hit.collider.gameObject.layer == LayerMask.NameToLayer(BOUNCYWALLLAYER))
+        {
+            points[recursionStep] = hit.point;
+            direction = Vector2.Reflect(direction, hit.normal);
+            hit = Physics2D.Raycast(hit.point, direction, float.MaxValue);
+            return ReflectRay(hit, direction, points, recursionStep + 1);
+        }
+        // Выстрел по любому шару/границе уничтожения - можно дальше не отражать
+        for(int i = recursionStep; i < MAXRECURSIONSTEP; i++)
+        {
+            points[recursionStep] = hit.point;
+        }
+        return points;
     }
 }
